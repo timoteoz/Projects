defab095f6023f56268c68b4e5dea62681f27efc
timoteo <mitife.miguel@gmail.com>
2026-02-05 15:59:13 -0300

    Projetos

--- Pomodoro Timer/assets/script.js
+++ Pomodoro Timer/assets/script.js
@@ -1,18 +1,50 @@
 const timerEl = document.getElementById("timer");
-const botaoEl = document.getElementById("botao");
+const startEl = document.getElementById("start");
+const resetEl = document.getElementById("reset")
+const pauseEl = document.getElementById("pause")
+const sound = new Audio('radio.mp3')
 
-const contagem = () => {
-   let tempo = Number(timerEl.value);
+let interval;
+let timeLeft = 1500;
 
-   const intervalo = setInterval(function() {
-      if (tempo <= 0) {
-        clearInterval(intervalo);
-        return;
-      }
+function updateTimer() {
+   let minutes = Math.floor(timeLeft / 60);
+   let seconds = timeLeft % 60;
+   let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2,"0")}`;
 
-      tempo = tempo - 1;
-      timerEl.value = tempo;
-   }, 1000)
+   timerEl.innerHTML = formattedTime;
 }
 
-botaoEl.addEventListener("click" , contagem);
+function startTimer() {
+   interval = setInterval(() => {
+      timeLeft--;
+      updateTimer();
+      if (timeLeft === 0) {
+         clearInterval(interval);
+         alert("Time's up!")
+         timeLeft = 1500;
+         updateTimer();
+      }
+   }, 1000);
+}
+function stopTimer() {
+   clearInterval(interval);
+}
+function resetTimer() {
+   clearInterval(interval);
+   timeLeft = 1500;
+   updateTimer();
+}
+
+startEl.addEventListener("click", () => {
+    startTimer();
+    sound.play();
+});
+resetEl.addEventListener("click" , () => {
+  resetTimer();
+  sound.play();  
+});
+pauseEl.addEventListener("click" , () => {
+   stopTimer();
+   sound.play();
+})
